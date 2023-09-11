01783e01d55f377a7a3e1d2a8d9dbbbf2edd9103
Prudhvi Godithi <pgodithi@amazon.com>
2023-09-11 13:22:22 -0700

    Relax cluster setting regex
    Signed-off-by: Prudhvi Godithi <pgodithi@amazon.com>

--- provider/resource_opensearch_cluster_settings_test.go
+++ provider/resource_opensearch_cluster_settings_test.go
@@ -85,6 +85,6 @@ func checkOpensearchClusterSettingsDestroy(s *terraform.State) error {
 var testAccOpensearchClusterSettings = `
 resource "opensearch_cluster_settings" "global" {
   cluster_max_shards_per_node = 10
-  action_auto_create_index    = "my-index-000001,index10,-index1*,+ind*"
+  action_auto_create_index    = "my-index-000001,index10,-index1*,+ind*,-.aws_cold_catalog*,+*"
 }
 `
