0019f2e85c38301cea75635a02174aa87dffcefd
Rupa Lahiri <rupacoach@gmail.com>
2023-10-18 19:23:16 +0530

    Add test for update
    Signed-off-by: Rupa Lahiri <rupacoach@gmail.com>

--- provider/resource_opensearch_anomaly_detection_test.go
+++ provider/resource_opensearch_anomaly_detection_test.go
@@ -23,6 +23,13 @@ func TestAccOpensearchAnomalyDetection(t *testing.T) {
 				),
 				ExpectNonEmptyPlan: true,
 			},
+			{
+				Config: testAccOpensearchAnomalyDetectionUpdate,
+				Check: resource.ComposeTestCheckFunc(
+					testCheckOpensearchAnomalyDetectionExists("opensearch_anomaly_detection.test-detector12"),
+				),
+				ExpectNonEmptyPlan: true,
+			},
 		},
 	})
 }
@@ -77,6 +84,62 @@ resource "opensearch_anomaly_detection" "test-detector12" {
 {
   "name": "test-detector12",
   "description": "Test detector",
+  "time_field": "@timestamp",
+  "indices": [
+    "security-auditlog*"
+  ],
+  "feature_attributes": [
+    {
+      "feature_name": "test",
+      "feature_enabled": true,
+      "aggregation_query": {
+        "test": {
+          "value_count": {
+            "field": "audit_category.keyword"
+          }
+        }
+      }
+    }
+  ],
+  "filter_query": {
+    "bool": {
+      "filter": [
+        {
+          "range": {
+            "value": {
+              "gt": 1
+            }
+          }
+        }
+      ],
+      "adjust_pure_negative": true,
+      "boost": 1
+    }
+  },
+  "detection_interval": {
+    "period": {
+      "interval": 1,
+      "unit": "Minutes"
+    }
+  },
+  "window_delay": {
+    "period": {
+      "interval": 1,
+      "unit": "Minutes"
+    }
+  },
+  "result_index" : "opensearch-ad-plugin-result-test"
+}
+EOF
+}
+`
+
+var testAccOpensearchAnomalyDetectionUpdate = `
+resource "opensearch_anomaly_detection" "test-detector12" {
+  body = <<EOF
+{
+  "name": "test-detector12",
+  "description": "Test detector 12",
   "time_field": "@timestamp",
   "indices": [
     "security-auditlog*"
