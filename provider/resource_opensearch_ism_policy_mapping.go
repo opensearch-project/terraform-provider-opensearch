d8e81eb1fb912b2e2a9edc3a9993e9e71a4d269c
Rupa Lahiri <rupacoach@gmail.com>
2024-02-26 23:05:03 +0530

    Change deprecation message for opensearch_ism_policy_mapping (#159)
    Signed-off-by: Rupa Lahiri <rupacoach@gmail.com>

--- provider/resource_opensearch_ism_policy_mapping.go
+++ provider/resource_opensearch_ism_policy_mapping.go
@@ -66,7 +66,7 @@ func resourceOpenSearchISMPolicyMapping() *schema.Resource {
 			Create: schema.DefaultTimeout(5 * time.Minute),
 			Update: schema.DefaultTimeout(5 * time.Minute),
 		},
-		DeprecationMessage: "opensearch_ism_policy_mapping is deprecated in Opensearch 1.x please use ism_template attribute in policies instead.",
+		DeprecationMessage: "opensearch_ism_policy_mapping is deprecated in Opensearch 1.x please use the opensearch_ism_policy resource and specify the ism_template attribute in the policies instead.",
 	}
 }
 
