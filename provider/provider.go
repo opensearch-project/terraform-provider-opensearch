5fb213f7495ab066cab96c04952b9023a8ec9a04
Rupa Lahiri <rupacoach@gmail.com>
2024-04-01 15:28:48 +0000

    Repeat the check for token within the check for insecure
    Signed-off-by: Rupa Lahiri <rupacoach@gmail.com>

--- provider/provider.go
+++ provider/provider.go
@@ -342,10 +342,13 @@ func getClient(conf *ProviderConf) (*elastic7.Client, error) {
 			return nil, err
 		}
 		opts = append(opts, elastic7.SetHttpClient(client), elastic7.SetSniff(false))
-	} else if conf.token != "" {
-		opts = append(opts, elastic7.SetHttpClient(tokenHttpClient(conf, map[string]string{})), elastic7.SetSniff(false))
 	} else if conf.insecure || conf.cacertFile != "" {
 		opts = append(opts, elastic7.SetHttpClient(tlsHttpClient(conf, map[string]string{})), elastic7.SetSniff(false))
+		if conf.token != "" {
+			opts = append(opts, elastic7.SetHttpClient(tokenHttpClient(conf, map[string]string{})), elastic7.SetSniff(false))
+		}
+	} else if conf.token != "" {
+		opts = append(opts, elastic7.SetHttpClient(tokenHttpClient(conf, map[string]string{})), elastic7.SetSniff(false))
 	} else {
 		opts = append(opts, elastic7.SetHttpClient(defaultHttpClient(conf, map[string]string{})))
 	}
